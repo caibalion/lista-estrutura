842a8959297aec3171318aad9b87c25c8acd17dd
Lucas Alex <lucasalexti@gmail.com>
2025-02-27 09:55:02 -0300

    question 02

--- src/Q2.java
+++ src/Q2.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
+
 public class Q2 {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        a = sc.nextInt();
+        System.out.print("Enter an integer: ");
+        b = sc.nextInt();
+        if(a == b){
+            System.out.println(a + " is equal to " + b);
+        }else if(a>b){
+            System.out.println(a + " is greater than " + b);
+        }else {
+            System.out.println(a + " is less than " + b);
+        }
+    }
 }
