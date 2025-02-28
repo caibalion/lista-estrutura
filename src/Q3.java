ef51a826a573a082b2d6bd43d5027c083ac222af
Lucas Alex <lucasalexti@gmail.com>
2025-02-28 10:33:03 -0300

    question 03

---
+++ src/Q3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Q3 {
+public static void main(String[] args) {
+    int a, b, c;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the first number");
+    a = sc.nextInt();
+    System.out.println("Enter the second number");
+    b = sc.nextInt();
+    c = a*b;
+    System.out.println("The product is " + c);
+}
+}
